e37acc813b44cdbd3a86df6ca9f8e63443bca639
Sebasan07 <juansebastiansp@ufps.edu.co>
2021-10-27 17:34:44 -0500

    microservicio-administrador

---
+++ microservicio-administrador/src/main/java/com/soltec/controllers/AdministradorController.java
@@ -0,0 +1,10 @@
+package com.soltec.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Controlador de tipo Rest
+@RequestMapping("/administrador")//Se accede a través de esta URL
+public class AdministradorController {
+
+}
