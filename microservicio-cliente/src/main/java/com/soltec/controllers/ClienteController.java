4eeb654b7793eca9d769ce65adcf8d8091cea9d2
Camila Calderon <angelycamilacg@ufps.edu.co>
2021-11-05 09:04:33 -0500

    vistas de cliente (listar y agregar)

--- microservicio-cliente/src/main/java/com/soltec/controllers/ClienteController.java
+++ microservicio-cliente/src/main/java/com/soltec/controllers/ClienteController.java
@@ -16,9 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.soltec.entities.Cliente;
 import com.soltec.service.ClienteService;
@@ -31,20 +28,30 @@ public class ClienteController {
 	
 	//listar los clientes
 	@GetMapping("/clientes")
-	public String index(Model model) {
+	public String inicio(Model model) {
 		model.addAttribute("list", clientService.findAll());
 		return "Dashboard/ver-clientes";
 	}
 	//agregar
-	@PostMapping("/crear-cliente")
-	public String crear(@RequestBody Cliente cliente, Model model) {
-		clientService.save(cliente);	
+	@GetMapping("/clientes-crear")
+	public String mostrarFormulario(Model model) {
+		Cliente c = new Cliente();
+		model.addAttribute("cliente", c);
 		return "Dashboard/agregar-cliente";	
 	}
 	
+	@PostMapping("clientes-creado")
+	public String crearCliente(@RequestBody Cliente cliente, Model model) {
+		clientService.save(cliente);	
+		return "redirect:/clientes";	
+	}
+	
 	//eliminar
-	
-	
+//	@GetMapping("/clientes-delete/{NIT}") //@DeleteMapping("/{NIT}")
+//	public String eliminar(@PathVariable(value="NIT") Integer NITcliente){
+//		clientService.deleteById(NITcliente);
+//		return "redirect:/clientes";
+//	}
 	//editar
 	
 	
@@ -52,40 +59,35 @@ public class ClienteController {
 	
 	
 	//Editar un usuario
-	@PutMapping("/{NIT}")
-	public ResponseEntity<?> editar(@RequestBody Cliente clienteEditar, @PathVariable(value="NIT") Integer NITcliente){
-		Optional<Cliente> cliente = clientService.findById(NITcliente);
-		if (!cliente.isPresent()) {
-			return ResponseEntity.notFound().build(); 
-		}	
-		cliente.get().setNumero_documento(clienteEditar.getNumero_documento());
-		cliente.get().setNombre_comercial(clienteEditar.getNombre_comercial());
-		cliente.get().setNombre(clienteEditar.getNombre());
-		cliente.get().setPais(clienteEditar.getPais());
-		cliente.get().setDepartamento(clienteEditar.getDepartamento());
-		cliente.get().setCiudad(clienteEditar.getCiudad());
-		cliente.get().setDireccion(clienteEditar.getDireccion());
-		cliente.get().setCorreo(clienteEditar.getCorreo());
-		cliente.get().setTelefono(clienteEditar.getTelefono());
-		cliente.get().setContribuyente(clienteEditar.getContribuyente());
-		cliente.get().setRegimen_contable(clienteEditar.getRegimen_contable());
-		cliente.get().setTipo_documento(clienteEditar.getTipo_documento());
-		return ResponseEntity.status(HttpStatus.CREATED).body(clientService.save(cliente.get()));
-	}
-	
-	//Eliminar un cliente
-	@DeleteMapping("/{NIT}")
-	public String eliminar(@PathVariable(value="NIT") Integer NITcliente){
-		clientService.deleteById(NITcliente);
-		return "redirect:/";
-	}
+//	@PutMapping("/{NIT}")
+//	public ResponseEntity<?> editar(@RequestBody Cliente clienteEditar, @PathVariable(value="NIT") Integer NITcliente){
+//		Optional<Cliente> cliente = clientService.findById(NITcliente);
+//		if (!cliente.isPresent()) {
+//			return ResponseEntity.notFound().build(); 
+//		}	
+//		cliente.get().setNumero_documento(clienteEditar.getNumero_documento());
+//		cliente.get().setNombre_comercial(clienteEditar.getNombre_comercial());
+//		cliente.get().setNombre(clienteEditar.getNombre());
+//		cliente.get().setPais(clienteEditar.getPais());
+//		cliente.get().setDepartamento(clienteEditar.getDepartamento());
+//		cliente.get().setCiudad(clienteEditar.getCiudad());
+//		cliente.get().setDireccion(clienteEditar.getDireccion());
+//		cliente.get().setCorreo(clienteEditar.getCorreo());
+//		cliente.get().setTelefono(clienteEditar.getTelefono());
+//		cliente.get().setContribuyente(clienteEditar.getContribuyente());
+//		cliente.get().setRegimen_contable(clienteEditar.getRegimen_contable());
+//		cliente.get().setTipo_documento(clienteEditar.getTipo_documento());
+//		return ResponseEntity.status(HttpStatus.CREATED).body(clientService.save(cliente.get()));
+//	}
+
+
 	
 	//Listar todos los usuarios
-	@GetMapping
-	public List<Cliente> leerTodos(){
-		List<Cliente> cliente = StreamSupport.stream(clientService.findAll().spliterator(),false).collect(Collectors.toList());
-		return cliente;
-	}
+//	@GetMapping
+//	public List<Cliente> leerTodos(){
+//		List<Cliente> cliente = StreamSupport.stream(clientService.findAll().spliterator(),false).collect(Collectors.toList());
+//		return cliente;
+//	}
 	
 	
 }
